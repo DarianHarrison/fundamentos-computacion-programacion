cac85c496a8684b65f91d750d046f880b1d444a5
Darian Harrison <darianharrison89@gmail.com>
2021-11-19 19:36:49 -0600

    Create ejemplos_python.py

---
+++ 3.programación/ejemplos_python.py
@@ -0,0 +1,69 @@
+# # variables
+# x = True
+# print(x)
+
+# # tipos
+# x = "Hello World" # str	
+# print(type(x))
+# x = 20	# int
+# print(type(x))
+# x = 20.5 # float	
+# print(type(x))
+# x = ["apple", "banana", "cherry"] # list, vector, matrix, array
+# print(type(x))
+# x = ("apple", "banana","banana","banana") # tuple, triple
+# print(type(x))
+# x = {"name" : "John", "age" : 36, "apellido": "Smith"} # dict	
+# print(type(x))
+# x = {"apple", "banana", "cherry"} #	set	
+# print(type(x))
+# x = True # bool	
+
+# # Operaciones
+# x = "hola "
+# y = "mundo"
+# z = x + y
+# print(z)
+
+# # Condicional
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+
+# # For loop
+# for x in range(10):
+#   print("hola mundo")
+
+
+# # Función
+# def suma(x,y):
+#   z = x + y
+#   return z
+
+# x = 7
+# y = 3
+
+# abc = suma(x,y)
+# print(abc)
+
+
+
+
+# # Objecto
+# class Person:
+
+#   def __init__(mysillyobject, name, age):
+#     mysillyobject.name = name
+#     mysillyobject.age = age
+
+#   def myfunc(abc):
+#     print("Hello my name is " + abc.name)
+
+# p1 = Person("John", 36)
+# p1.myfunc()
